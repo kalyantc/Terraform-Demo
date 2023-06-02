f5902a2851833b85ad82b2b3f467b4bec1fdee26
kalyantc <37632082+kalyantc@users.noreply.github.com>
2023-06-02 13:11:58 -0400

    Delete terraform directory

--- terraform/resource_groups.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "rg_networking" {
-  name     = "rg-${var.application}-${var.environment}-networking-001"
-  location = var.location
-}
-
-resource "azurerm_resource_group" "rg_compute" {
-  name     = "rg-${var.application}-${var.environment}-compute-001"
-  location = var.location
-}
